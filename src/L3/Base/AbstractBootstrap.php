1d0483575cf7409dd103e7d73e9d70a0fad49ca5
designinlife <wuxuexuan@msn.com>
2015-12-13 01:24:11 +0800

    init

---
+++ src/L3/Base/AbstractBootstrap.php
@@ -0,0 +1,13 @@
+<?php
+namespace L3\Base;
+
+/**
+ * Class `l3-base` Abstract Bootstrap.
+ *
+ * @package       L3\Base
+ * @author        Lei Lee <web.developer.network@gmail.com>
+ * @version       1.0.0
+ * @copyright (c) 2005-2015, Lei Lee
+ */
+class AbstractBootstrap {
+}
